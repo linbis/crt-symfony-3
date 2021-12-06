e5086635b07f5294b7c05727ff5fd880a3530922
Ivan Lyvin <ivan.lyvin@gmail.com>
2021-12-06 23:36:44 +0300

    #dev ~ установлены права на доступ

--- app/src/Controller/Admin/DashboardController.php
+++ app/src/Controller/Admin/DashboardController.php
@@ -9,10 +9,8 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
-/**
- * @IsGranted("ROLE_ADMIN")
- * @IsGranted("ROLE_MANAGER")
- */
+#[IsGranted('ROLE_ADMIN')]
+#[IsGranted('ROLE_EDITOR')]
 class DashboardController extends AbstractDashboardController
 {
     #[Route('/admin', name: 'admin')]
