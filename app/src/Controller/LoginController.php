c2194d566fbedc4576565ed36dcc0e833bc3bd4c
Ivan Lyvin <ivan.lyvin@gmail.com>
2021-12-10 15:10:14 +0300

    #dev2 ~ add logout

--- app/src/Controller/LoginController.php
+++ app/src/Controller/LoginController.php
@@ -21,4 +21,10 @@ class LoginController extends AbstractController
             'error'         => $error,
         ]);
     }
+
+    #[Route('/logout', name: 'logout')]
+    public function logout(): void
+    {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
 }
