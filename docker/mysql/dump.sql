72fd7dede1c69cde6200338755bc85c563ef3a2f
Ivan Lyvin <ivan.lyvin@gmail.com>
2021-12-12 22:36:14 +0300

    #dev ~

--- docker/mysql/dump.sql
+++ docker/mysql/dump.sql
@@ -1,3 +1,15 @@
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
 -- auto-generated definition
 create table if not exists articles
 (
@@ -10,8 +22,7 @@ create table if not exists articles
     publish_time datetime     not null,
     created_at   datetime     not null,
     updated_at   datetime     null
-)
-    collate = utf8mb4_unicode_ci;
+) ENGINE=InnoDB CHARSET=utf8;
 
 INSERT INTO `crt-symfony-3`.articles (id, author, title, slug, anons, content, publish_time, created_at, updated_at) VALUES (1, 'vc.ru', 'До слова «термос» была компания Thermos', 'do-slova-termos-byla-kompaniya-thermos', 'История бренда, который внедрил новое слово и судился за него с другими. Всё началось с немца Рейнгольда Бургера — он взял придуманную Джеймсом Дьюаром вакуумную колбу и сделал из неё товар.', 'Всё началось с немца Рейнгольда Бургера — он взял придуманную Джеймсом Дьюаром вакуумную колбу и сделал из неё товар. С тех пор прошло больше ста лет, в которых были и суды, и экcперименты с новыми продуктами, и продажа Thermos.
 
@@ -53,8 +64,7 @@ create table if not exists users
     password varchar(255) not null,
     name     varchar(255) not null,
     constraint UNIQ_1483A5E9E7927C74 unique (email)
-)
-    collate = utf8mb4_unicode_ci;
+) ENGINE=InnoDB CHARSET=utf8;
 
 INSERT INTO `crt-symfony-3`.users (id, email, roles, password, name) VALUES (1, 'admin@mail.ru', '["ROLE_ADMIN"]', '$2y$13$ZtOMM7T5mpA6qNBuo3ZuS.W1RhWXWhN51DVeqbciUOGsv8PbdVHS.', 'admin');
 INSERT INTO `crt-symfony-3`.users (id, email, roles, password, name) VALUES (2, 'manager@mail.ru', '["ROLE_EDITOR"]', '$argon2id$v=19$m=65536,t=4,p=1$MqzFN6RzYn8xnRmkqWjs2g$JiizY4EJxLGkREb4J4cbi6yhpYWbsFen1B5VR4lL6fY', 'manager');
@@ -64,13 +74,11 @@ create table if not exists migration_versions
 (
     version     varchar(14) not null primary key,
     executed_at datetime    not null comment '(DC2Type:datetime_immutable)'
-)
-    collate = utf8mb4_unicode_ci;
+) ENGINE=InnoDB CHARSET=utf8;
 
 INSERT INTO `crt-symfony-3`.migration_versions (version, executed_at) VALUES ('20211206193139', '2021-12-12 14:56:43');
 INSERT INTO `crt-symfony-3`.migration_versions (version, executed_at) VALUES ('20211210114910', '2021-12-12 15:07:39');
 INSERT INTO `crt-symfony-3`.migration_versions (version, executed_at) VALUES ('20211210122407', '2021-12-12 14:56:43');
-
 
 -- auto-generated definition
 create table if not exists pages
@@ -80,11 +88,19 @@ create table if not exists pages
     text       longtext     not null,
     created_at datetime     not null,
     updated_at datetime     null
-)
-    collate = utf8mb4_unicode_ci;
+) ENGINE=InnoDB CHARSET=utf8;
 
 INSERT INTO `crt-symfony-3`.pages (id, title, text, created_at, updated_at) VALUES (1, 'About Me', '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe nostrum ullam eveniet pariatur voluptates odit, fuga atque ea nobis sit soluta odio, adipisci quas excepturi maxime quae totam ducimus consectetur?</p>
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius praesentium recusandae illo eaque architecto error, repellendus iusto reprehenderit, doloribus, minus sunt. Numquam at quae voluptatum in officia voluptas voluptatibus, minus!</p>
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut consequuntur magnam, excepturi aliquid ex itaque esse est vero natus quae optio aperiam soluta voluptatibus corporis atque iste neque sit tempora!</p>', '2021-12-12 18:49:11', '2021-12-12 21:56:09');
 INSERT INTO `crt-symfony-3`.pages (id, title, text, created_at, updated_at) VALUES (2, 'Robots.txt', 'Disallow: /', '2021-12-12 21:56:33', null);
 
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
